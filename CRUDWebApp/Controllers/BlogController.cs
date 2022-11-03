75e5c28f396a690b35cedd69ad25697fc40818e9
Jan Świder <jasiek.swider@outlook.com>
2022-11-04 00:32:10 +0100

    blog dodawanie postów

---
+++ CRUDWebApp/Controllers/BlogController.cs
@@ -0,0 +1,38 @@
+﻿using CRUDWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDWebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        public static List<Post> posts = new List<Post>();
+        public static int count = 0;
+        public IActionResult Index()
+        {
+            return View(posts);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View("PostForm");
+        }
+        [HttpPost]
+        public IActionResult Add(Post post,[FromForm(Name = "image")] IFormFile image)
+        {
+            //if(!ModelState.IsValid)
+            //    return View("PostForm");
+
+            if(image is not null)
+            {
+                post.Image = new byte[image.Length];
+                image.OpenReadStream().Read(post.Image, 0, (int)image.Length);
+            }
+            post.Id = count++;
+
+            posts.Add(post);
+            return View("index",posts);
+        }
+
+    }
+}
---
+++ CRUDWebApp/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDWebApp.Models
+{
+    public class Post
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        //[Required(ErrorMessage = "Podaj tytuł")]
+        public string Title { get; set; }
+        [HiddenInput]
+        public byte[] Image { get; set; }
+        public string Description { get; set; }
+
+    }
+}
